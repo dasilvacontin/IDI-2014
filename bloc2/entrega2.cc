9626d54a0a32f2a61d18f1070c596e578535865b
David da Silva <daviddasilvacontin@me.com>
2014-03-07 09:59:12 +0100

    Entrega 2 progress

---
+++ bloc2/entrega2.cc
@@ -0,0 +1,249 @@
+#include "GLIncludes.h"
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../Model/model.h"
+
+#include "GameModel.h"
+#include "GameObject.h"
+
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+using namespace std;
+
+int mx = 0, my = 0, edge = 600;
+int rotX = 30, rotY = 30;
+int traX = 0, traY = 0;
+float scaleX = 1, scaleY = 1;
+float xView = 0, yView = 0;
+
+const char* WINDOW_NAME = "Hello World";
+
+const string HOMER_MODEL_PATH = "../Model/HomerProves.obj";
+const string RAYMAN_MODEL_PATH = "../objects/Rayman3/Rayman3.obj";
+const string LEGOMAN_MODEL_PATH = "../Model/legoman.obj";
+
+int windowIndentifier;
+
+const int SPACE_KEY = 32;
+const int T_KEY = 116;
+const int R_KEY = 114;
+const int S_KEY = 115;
+const int C_KEY = 99;
+const int H_KEY = 104;
+const int ESC_KEY = 27;
+
+const int ROT_STATE = 0;
+const int TRANS_STATE = 1;
+const int SCALE_STATE = 2;
+const int HERO_STATE = 3;
+int currentState = 0;
+vector<string> states;
+
+Model m;
+
+void nextState () {
+    currentState = ++currentState % states.size();
+}
+
+GameObject legoman;
+
+void renderAxis (int size) {
+
+	glPushMatrix();
+
+	glBegin(GL_LINES);
+
+	glColor4f(1,0,0,1);
+	glVertex3f(0,0,0);
+	glVertex3f(size,0,0);
+
+	glColor4f(0,1,0,1);
+	glVertex3f(0,0,0);
+	glVertex3f(0,size,0);
+	
+	glColor4f(0,0,1,1);
+	glVertex3f(0,0,0);
+	glVertex3f(0,0,size);
+
+	glEnd();
+
+	glPopMatrix();
+
+}
+
+void renderScene (void) {
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glPushMatrix();
+
+	//Camera transformations
+	glTranslated(traX/500.0, traY/500.0, 0);
+	glScaled(scaleX, scaleX, 1);
+	glRotated(rotX/2.0, 0, 1, 0);
+	glRotated(rotY/2.0, 1, 0, 0);
+
+	//Stage axis render
+	renderAxis(1);
+
+    //Hero render
+   	legoman.render();
+	
+	//Floor render
+	glColor4f((float)0x7C/0xFF, (float)0xD2/0xFF, (float)0x79/0xFF, 1);
+	glBegin(GL_QUADS);
+		glVertex3f(-1.5, -0.4, -1.5);
+		glVertex3f(-1.5, -0.4, 1.5);
+		glVertex3f(1.5, -0.4, 1.5);
+		glVertex3f(1.5, -0.4, -1.5);
+	glEnd();
+
+	glPopMatrix();
+	glutSwapBuffers();
+
+}
+
+void reshapeWindow (int w, int h) {
+	edge = std::min(w,h);
+	xView = (w-edge)/2.0;
+	yView = (h-edge)/2.0;
+	glViewport(xView,yView,edge,edge);
+}
+
+void mouseEvent (int button, int state, int x, int y) {
+	mx = x;
+	my = y;
+}
+
+void renderUI () {
+	
+	system("clear");
+   	cout << endl << "-- ENTREGA2 VIEWER --" << endl << endl;
+
+   	for (int i = 0; i < states.size(); ++i) {
+   		cout << "[";
+   		if (i == currentState) cout << "*";
+   		else cout << " ";
+   		cout << "] " << states[i] << endl;
+   	}
+
+   	cout << endl << "R:(" << rotX << "," << rotY << "), T:(" << traX << "," << traY << "), S:(" << scaleX << ")" << endl << endl;
+
+   	cout << "ESC to exit." << endl << endl;
+
+}
+
+void mouseMove (int x, int y) {
+
+	switch(currentState) {
+
+		case ROT_STATE:
+			rotX += x - mx;
+			rotY += y - my;
+			break;
+
+		case TRANS_STATE:
+			traX += x - mx;
+			traY += my - y;
+			break;
+		
+		case SCALE_STATE:
+			scaleX *= (x-mx)/100.0 + 1;
+			scaleY *= (y-my)/100.0 + 1;
+			break;
+
+		case HERO_STATE:
+			legoman.p[0] += (x-mx)/100.0;
+			legoman.p[2] += (y-my)/100.0;
+			break;
+
+	}
+
+	mx = x;
+	my = y;
+	glutPostRedisplay();
+	renderUI();
+
+}
+
+void keyboardEvent (unsigned char key, int x, int y) {
+
+    int oldState = currentState;
+
+    //cout << (int)key << endl;
+
+    switch((int)key) {
+
+        case SPACE_KEY: nextState();
+            break;
+
+        case T_KEY: currentState = TRANS_STATE;
+            break;
+
+        case R_KEY: currentState = ROT_STATE;
+            break;
+
+        case S_KEY: currentState = SCALE_STATE;
+            break;
+
+        case H_KEY: currentState = HERO_STATE;
+        	break;
+
+        case ESC_KEY: cout << "KTHXBYE" << endl << endl;
+        	exit(0);
+        	break;
+
+    }
+
+   if (oldState != currentState) {
+   		renderUI();
+   }
+
+}
+
+int main (int argc, const char * argv []) {
+
+	srand (time(NULL));
+
+	states.push_back("Rotate! (R)");
+	states.push_back("Translate! (T)");
+	states.push_back("Scale! (S)");
+	states.push_back("Hero! (H)");
+	nextState();
+
+	GameModel legomanModel (LEGOMAN_MODEL_PATH);
+	legoman = GameObject(legomanModel);
+
+	legoman.p[1] = -0.4 + 0.5/2; //Feet position + half height
+
+	legoman.s[0] = legoman.s[1] = legoman.s[2] = 0.5*0.5;
+
+	glutInit(&argc, (char **)argv);
+    	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(edge,edge);
+	windowIndentifier = glutCreateWindow(WINDOW_NAME);
+    	glEnable(GL_DEPTH_TEST);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1,1,-1,1,-1,1);
+	glMatrixMode(GL_MODELVIEW);
+    	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glClearColor(255, 255, 255, 1);
+
+	glutReshapeFunc(reshapeWindow);
+	glutDisplayFunc(renderScene);
+	glutMouseFunc(mouseEvent);
+	glutMotionFunc(mouseMove);
+	glutKeyboardFunc(keyboardEvent);
+
+	renderUI();
+
+	glutMainLoop();
+
+  return 0;
+}
